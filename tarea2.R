afbed5304b68dba9b28ff9a866a8498068f564e4
MatiasMatus <43860654+MatiasMatus@users.noreply.github.com>
2019-05-17 15:38:59 -0400

    Delete tarea2.R

--- tarea2.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(readr)
-
-
-datos <- read_csv("../Curso R/googleplaystore.csv")
-datos %>% 
- ggplot(aes(x=Rating),!is.na(datos$Rating)) + 
-  geom_density(color="darkblue", fill="lightblue")
